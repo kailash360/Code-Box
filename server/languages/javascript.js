6c08198829db0b50af118dc313308661da30cf89
kailash360 <kailashkejriwal21@gmail.com>
2022-04-07 01:34:17 +0530

    added javascript handler

---
+++ server/languages/javascript.js
@@ -0,0 +1,35 @@
+// handler for js programs
+
+// package imports
+const isValid = require('../lib/valid')
+const execute = require('../lib/execute')
+
+//constants
+const CONSTANTS = require('../constants.js')
+
+// execution handling
+const jsHandler = async(key, storagePath) => {
+    const filePath = storagePath + key
+
+    // RegExp for require(<foo>) statement sanitisation
+    const importRE = CONSTANTS.JS.REGEX
+
+    // list of acceptable libraries
+    const acceptList = CONSTANTS.JS.ACCEPT_LIST
+
+    // code validity check
+    if (!(await isValid(filePath + '.js', false, acceptList, importRE, ''))) {
+        return {
+            code: 1,
+            stdout: '',
+            stderr: 'invalid code',
+        }
+    }
+
+    // at this point, we have validated the code and run execute()
+    const result = await execute('js', filePath)
+    return result
+}
+
+// code handler exported
+module.exports = jsHandler
