7582e3a2287d0e68effa4edeb50e8bae3b2b6bc4
kailash360 <kailashkejriwal21@gmail.com>
2022-04-07 01:32:13 +0530

    added socket

---
+++ server/socket/index.js
@@ -0,0 +1,18 @@
+const { socketEvents } = require('./socketEvents')
+const CONSTANTS = require('../constants.js')
+const SOCKET = CONSTANTS.SOCKET
+
+module.exports.initializer = (socketIo, server) => {
+    const io = socketIo(server, {
+        cors: {
+            origin: SOCKET.ORIGIN,
+            methods: SOCKET.METHODS,
+            credentials: true,
+        },
+    })
+
+    // socket listens to connection event
+    io.on('connection', (client) => socketEvents(client, io))
+
+    return io
+}
