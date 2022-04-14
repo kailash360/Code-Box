e4c2b43c55e4a4b0300104566bf5ed6cbc74ce2a
kailash360 <kailashkejriwal21@gmail.com>
2022-04-15 00:03:38 +0530

    Modified use of local storage

--- client/src/hooks/useLocalStorage.js
+++ client/src/hooks/useLocalStorage.js
@@ -7,23 +7,23 @@ const CONSTANTS = require('../constants.js')
 // custom hook
 // stores state variables in localStorage as well when their value is set
 const useLocalStorage = (key, initialValue) => {
-  const prefixedKey = CONSTANTS.PREFIX + key
-  const [value, setValue] = useState(() => {
-    const jsonValue = localStorage.getItem(prefixedKey)
-    // if localStorage has a value at the key, return the parsed value
-    if (jsonValue != null) return JSON.parse(jsonValue)
-    // if initialValue is a function, return the evaluated value
-    if (typeof initialValue === 'function') return initialValue()
-    // else return the initialValue itself
-    else return initialValue
-  })
+    const prefixedKey = CONSTANTS.PREFIX + key
+    const [value, setValue] = useState(() => {
+        const jsonValue = localStorage.getItem(prefixedKey)
+            // if localStorage has a value at the key, return the parsed value
+        if (jsonValue != null) return JSON.parse(jsonValue)
+            // if initialValue is a function, return the evaluated value
+        if (typeof initialValue === 'function') return initialValue()
+            // else return the initialValue itself
+        else return initialValue
+    })
 
-  // handle changes by storing value in localStorage
-  useEffect(() => {
-    localStorage.setItem(prefixedKey, JSON.stringify(value))
-  }, [prefixedKey, value])
+    // handle changes by storing value in localStorage
+    useEffect(() => {
+        localStorage.setItem(prefixedKey, JSON.stringify(value))
+    }, [prefixedKey, value])
 
-  return [value, setValue]
+    return [value, setValue]
 }
 
 export default useLocalStorage
